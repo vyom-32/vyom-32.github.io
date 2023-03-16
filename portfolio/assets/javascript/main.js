b010949acd8ca2be770236f0ec4b920217ed08af
Vyom Sarsavadiya <vyom.s@frendy.in>
2023-03-16 22:21:00 +0530

    new ui changes

--- portfolio/assets/javascript/main.js
+++ portfolio/assets/javascript/main.js
@@ -7,6 +7,8 @@ $(".projects-wrapper").slick({
   prevArrow: $(".projects-prev"),
 });
 
+// console.log = () => {};
+
 const menuBtn = document.getElementById("menuBtn");
 nav = document.querySelector(".nav");
 menuBtnExit = document.getElementById("closeMenu");
@@ -24,7 +26,7 @@ menuBtnExit.addEventListener("click", () => {
 class Slider {
   constructor(options) {
     this.sections = document.querySelectorAll(options.section);
-    console.log(this.sections);
+    // console.log(this.sections);
     this.navigation = document.querySelector(options.dots);
 
     this.navigation.addEventListener("click", this.scrollToSection.bind(this));
@@ -89,3 +91,62 @@ new Slider({
   section: ".new-section",
   dots: "#navDots",
 });
+
+const bgIcons = document.getElementsByClassName("bg-icons");
+let timeout;
+let i = 0;
+
+// document.onscroll = function (e) {
+//   console.log("scrolling", e)
+//   clearTimeout(timeout);
+//   for(let icon of bgIcons){
+//     console.log(i)
+//     icon.classList.add(i%2)
+//     if(i%2 == 1){
+//       icon.classList.add('rotating-animation-clockwise')
+//     }
+//     else{
+//       icon.classList.add('rotating-animation-anticlockwise')
+//     }
+//     icon.classList.remove('remove-rotating-animation')
+//     i++;
+//   }
+//   i = 0;
+//   timeout = setTimeout(function () {
+//     for(let icon of bgIcons){
+//       icon.classList.remove('rotating-animation-anticlockwise')
+//       icon.classList.remove('rotating-animation-clockwise')
+//       icon.classList.remove('1')
+//       icon.classList.remove('0')
+//       icon.classList.add('remove-rotating-animation')
+//     }
+//   }, 300);
+// }
+
+window.addEventListener("wheel", function (event) {
+  clearTimeout(timeout);
+  if (event.deltaY < 0) {
+    for (let icon of bgIcons) {
+      icon.classList.remove('scrolleddown')
+      icon.classList.add('scrolledup')
+    }
+  }
+  else if (event.deltaY > 0) {
+    for (let icon of bgIcons) {
+      icon.classList.remove('scrolledup')
+      icon.classList.add('scrolleddown')
+    }
+  }
+  timeout = setTimeout(function () {
+    for (let icon of bgIcons) {
+      icon.classList.remove('scrolleddown','scrolledup')
+    }
+  }, 3000);
+})
+
+// const bgIconsContainer = document.getElementsByClassName("bg-icons-container")
+
+// for(let icon of bgIcons){
+//   icon.style.top = Math.random() * bgIconsContainer[0].offsetHeight + "px";
+//   icon.style.left = Math.random() * bgIconsContainer[0].offsetWidth + "px";
+// }
